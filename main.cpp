29fd65cce912ceb85893812213d89a124f57847d
Александр Доленко <aleksandrdolenko@MacBook-Air-Aleksandr.local>
2025-10-20 12:57:13 +0300

    matrxi5

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@ int ** create(size_t rows,size_t cols);
 void remove(int ** m, size_t rows,size_t cols);
 void input(int ** m, size_t rows,size_t cols){
     for (size_t i=0;i<rows && std::cin;++i){
-        for (size_t j=0;j<cols&& std::cin;++j){
-            std::cin>>m[i][j]
+        for (size_t j=0;j<cols && std::cin;++j){
+            std::cin>>m[i][j];
         }
     }
 }
-void output(const * int * m, size_t rows,size_t cols);
+void output(const * int * m, size_t rows,size_t cols){
+    std::cout<<rows<<" "<<cols;
+}
 int main() {
     size_t rows=0,cols=0;
     std::cin>>rows>>cols;
