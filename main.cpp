936df822a3011b2f78e17ff9f7478a1a722dfa6a
Александр Доленко <aleksandrdolenko@MacBook-Air-Aleksandr.local>
2025-10-20 12:38:31 +0300

    matrix2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+
+int ** create(size_t rows,size_t cols);
+void remove(int ** m, size_t rows,size_t cols);
 int main() {
     size_t rows=0,cols=0;
     std::cin>>rows>>cols;
     if (!std::cin){
         return 1;
     }
+    int ** m = create(rows,cols);
     
     std::cout<<rows<<" "<<cols<<"\n";
+    remove(m,rows,cols);
 }
