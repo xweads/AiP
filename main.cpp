4590e16d9ea0c879bc5b43a5102be7c2dddc5f80
Александр Доленко <aleksandrdolenko@MacBook-Air-Aleksandr.local>
2025-10-20 12:54:07 +0300

    matrix4

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 
 int ** create(size_t rows,size_t cols);
 void remove(int ** m, size_t rows,size_t cols);
-void input(int ** m, size_t rows,size_t cols);
+void input(int ** m, size_t rows,size_t cols){
+    for (size_t i=0;i<rows && std::cin;++i){
+        for (size_t j=0;j<cols&& std::cin;++j){
+            std::cin>>m[i][j]
+        }
+    }
+}
 void output(const * int * m, size_t rows,size_t cols);
 int main() {
     size_t rows=0,cols=0;
     std::cin>>rows>>cols;
-    if (!std::cin){
-        return 1;
-    }
+    
     int ** m = create(rows,cols);
     input(m,rows,cols);
+    if (!std::cin){
+        remove(m,rows,cols);
+        return 1;
+    }
     output(m,rows,cols);
     remove(m,rows,cols);
     std::cout<<"\n";
